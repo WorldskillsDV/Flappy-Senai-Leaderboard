c8693380c3244e94a277bb4ac2c43d7a6095507b
Ferps53 <felipebrostolinribeiro@gmail.com>
2025-04-27 21:50:48 -0300

    Updated to use the new backend with redis

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,117 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import QrCode from "../public/qrcode.png";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
-    const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState([]);
 
-    useEffect(() => {
-        const createWebSocket = () => {
-            const socket = new WebSocket(
-                "wss://flappy-senai.up.railway.app/leaderboard-ws",
-            ); // URL do WebSocket
+  useEffect(() => {
+    const createWebSocket = () => {
+      const socket = new WebSocket(
+        "ws://localhost:8080/leaderboard-ws",
+      ); // URL do WebSocket
 
-            socket.onmessage = (event) => {
-                const data = JSON.parse(event.data);
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
 
-                if (data.players) {
-                    setPlayers(data.players);
-                } else if (data.playersWithAlteredPositions) {
-                    setPlayers((prevPlayers) => {
-                        const updatedPlayers = [...prevPlayers];
+        setPlayers((prevPlayers) => {
+          const updatedPlayers = [...prevPlayers];
 
-                        data.playersWithAlteredPositions.forEach(
-                            (updatedPlayer) => {
-                                const index = updatedPlayers.findIndex(
-                                    (player) => player.id === updatedPlayer.id,
-                                );
+          data.forEach((updatedPlayer) => {
+            const index = updatedPlayers.findIndex(
+              (player) => player.id === updatedPlayer.id,
+            );
 
-                                if (index !== -1) {
-                                    updatedPlayers[index] = updatedPlayer;
-                                } else {
-                                    updatedPlayers.push(updatedPlayer);
-                                }
-                            },
-                        );
+            if (index !== -1) {
+              updatedPlayers[index] = updatedPlayer;
+            } else {
+              updatedPlayers.push(updatedPlayer);
+            }
+          });
 
-                        return updatedPlayers;
-                    });
-                }
-            };
+          return updatedPlayers;
+        });
+      };
+      socket.onerror = (error) => {
+        console.error("Erro:", error);
+      };
 
-            socket.onerror = (error) => {
-                console.error("Erro:", error);
-            };
+      return socket;
+    };
 
-            return socket;
-        };
+    createWebSocket();
+  }, []);
 
-        createWebSocket();
-    }, []);
+  const sortedPlayers = [...players].sort((a, b) => b.highScore - a.highScore);
 
-    const sortedPlayers = [...players].sort((a, b) =>
-        b.highScore - a.highScore
-    );
+  return (
+    <div className="container">
+      <div className="column" style={{ backgroundColor: "#333" }}>
+        <h1 style={{ textAlign: "center" }}>Pontuação Flappy-Senai:</h1>
+        <div style={{ overflowY: "scroll" }}>
+          <ul className="player-list">
+            <AnimatePresence>
+              {sortedPlayers.length === 0
+                ? (
+                  <motion.li
+                    key="empty"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    Nenhum jogador encontrado
+                  </motion.li>
+                )
+                : (
+                  sortedPlayers.map((player, index) => {
+                    const position = index + 1;
 
-    return (
-        <div className="container">
-            <div className="column" style={{ backgroundColor: "#333" }}>
-                <h1 style={{ textAlign: "center" }}>Pontuação Flappy-Senai:</h1>
-                <div style={{ overflowY: "scroll" }}>
-                    <ul className="player-list">
-                        {sortedPlayers.length === 0
-                            ? <li>Nenhum jogador encontrado</li>
-                            : (
-                                sortedPlayers.map((player, index) => {
-                                    let className = "player-item";
-                                    if (index === 0) className += " gold";
-                                    // Primeiro lugar
-                                    else if (index === 1) {
-                                        className += " silver";
-                                    } // Segundo lugar
-                                    else if (index === 2) {
-                                        className += " bronze";
-                                    } // Terceiro lugar
-                                    else className += " other"; // Outros lugares
+                    let className = "player-item";
+                    if (index === 0) className += " gold";
+                    else if (index === 1) className += " silver";
+                    else if (index === 2) className += " bronze";
+                    else className += " other";
 
-                                    return (
-                                        <li
-                                            key={player.id}
-                                            className={className}
-                                        >
-                                            {player.position} -{" "}
-                                            <span
-                                                style={{ fontWeight: "bold" }}
-                                            >
-                                                {player.playerName}
-                                            </span>{" "}
-                                            - {player.highScore}
-                                        </li>
-                                    );
-                                })
-                            )}
-                    </ul>
-                </div>
-            </div>
-            <div className="column" style={{ backgroundColor: "#444" }}>
-                <div className="qr-code">
-                    <div className="title">Premios:</div>
-                    <div className="prize-list">1° Lugar: Tapa</div>
-                    <div className="prize-list">2° Lugar: Tapa</div>
-                    <div className="prize-list">3° Lugar: Tapa</div>
-
-                    <img src={QrCode} alt="QR Code" />
-                    <div className="qr-code-scan">
-                        Escaneie o QR Code para jogar!
-                    </div>
-                </div>
-            </div>
+                    return (
+                      <motion.li
+                        key={player.id}
+                        layout
+                        initial={{ opacity: 0, y: index * 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 10 }}
+                        transition={{ duration: 0.3 }}
+                        style={{ position: "relative", zIndex: 1 }}
+                        className={className}
+                      >
+                        {position} -{" "}
+                        <span style={{ fontWeight: "bold" }}>
+                          {player.playerName}
+                        </span>{" "}
+                        - {player.highScore}
+                      </motion.li>
+                    );
+                  })
+                )}
+            </AnimatePresence>
+          </ul>
         </div>
-    );
+      </div>
+      <div className="column" style={{ backgroundColor: "#444" }}>
+        <div className="qr-code">
+          <div className="title">Premios:</div>
+          <div className="prize-list">1° Lugar: Tapa</div>
+          <div className="prize-list">2° Lugar: Tapa</div>
+          <div className="prize-list">3° Lugar: Tapa</div>
+
+          <img src={QrCode} alt="QR Code" />
+          <div className="qr-code-scan">
+            Escaneie o QR Code para jogar!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
